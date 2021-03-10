06b2654394509c3e6691e6a9cab21524b80ca931
abhishekpatelmc <mcabhishekpatel2017@gmail.com>
2021-03-10 19:13:18 +0530

    commit 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Navbar from './Navbar' ;
 import Home from './Home' ;
+import {useEffect} from 'react'
 
 function App() {
   // const person = { name: 'abhi', age: 22 } ;
   // Objects can't be used as the child of react , react will not render objects and boolean
+ function a(){
+  console.log("hello")
+ }
+ useEffect(() => {
+    console.log("hello");
+   
+ }, [])
+
 
   return (
     <div className="App">
-      <Navbar />
+     
+      <Navbar function={a} />
       <div className="content">
       <Home />
       </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,13 @@
-const Navbar = () => {
+
+
+const Navbar = (props) => {
     return (  
         <nav className="navbar">
-            <h1>React Blog</h1>
+            <h1 >React Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create">New Blog</a>
+                <button onClick={props.function}>function</button>
 
             </div>
         </nav>
