32c5a39e4ddabce6a420b31932ea206bd4f10680
abhishekpatelmc <mcabhishekpatel2017@gmail.com>
2021-03-14 19:07:21 +0530

    updated

--- src/Home.js
+++ src/Home.js
@@ -2,18 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setblogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
-
-  const [name, setName] = useState('mario');
+  const [blogs, setblogs] = useState(null);
 
   const handelDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -27,10 +16,6 @@ const Home = () => {
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" handelDelete={handelDelete} />
-      <button onClick={() => {
-        setName('Abhi')
-      }}>Chane name </button>
-      <p>{name}</p>
     </div>
   );
 };
