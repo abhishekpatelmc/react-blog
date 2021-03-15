ec6f9dbd0d17d22a01488d7871218629111a8f2e
abhishekpatelmc <mcabhishekpatel2017@gmail.com>
2021-03-15 22:35:53 +0530

    updated

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,12 @@
+import { Link } from "react-router-dom";
 
-
-const Navbar = (props) => {
+const Navbar = (props)  => {
     return (  
         <nav className="navbar">
             <h1 >React Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
     );
